815b4a32654c23046add0c2b6b0537173578e5ad
fccoelho7 <fccoelho7@gmail.com>
2019-01-06 13:12:20 -0200

    Add initial antd locales

--- src/containers/Root.jsx
+++ src/containers/Root.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { LocaleProvider } from "antd";
+import enUS from "antd/lib/locale-provider/en_US";
 import Dashboard from "./Dashboard";
 import Posts from "./Posts";
 import NotFound from "./NotFound";
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={Dashboard} />
-        <Route path="/posts" component={Posts} />
-        <Route path="*" component={NotFound} />
-      </Switch>
-    </BrowserRouter>
+    <LocaleProvider locale={enUS}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Dashboard} />
+          <Route path="/posts" component={Posts} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+    </LocaleProvider>
   </Provider>
 );
 
